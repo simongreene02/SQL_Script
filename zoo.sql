e64ed5b2472d5bf0a15a267935a89f50b3a10553
Simon Greene <simongreene02@gmail.com>
2017-02-04 11:05:11 -0600

    Created CRUD operation

--- zoo.sql
+++ zoo.sql
@@ -1,4 +1,15 @@
 create table animals (
   species VARCHAR(15),
-  quantity NUMBER(2)
+  quantity INTEGER(2)
 );
+
+-- CRUD operation
+INSERT INTO animals(species, quantity) VALUES('wolves', 10);
+INSERT INTO animals(species, quantity) VALUES('penguins', 20);
+INSERT INTO animals(species, quantity) VALUES('bears', 5);
+
+select * from animals;
+
+update animals set quantity = 50 where species = 'bears';
+
+delete from animals where species = 'penguins';
